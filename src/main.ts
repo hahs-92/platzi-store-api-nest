b79e77bd7db312c0f76c90546781d529fd54ed62
hahs <alexandre_2420@outlook.com>
2022-11-09 15:44:46 -0500

    clean incorrect params

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,17 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   //agregamos validaciones globales
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //elimina los datos que no esten en el dto
+      forbidNonWhitelisted: true, // informa cuando nos envian datos extras
+
+      // EVITAR ENVIAR MENSAGES AL CLIENTE
+      // disableErrorMessages:
+      //   process.env.ENVIROMENT === 'production' ? true : false,
+      disableErrorMessages: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
