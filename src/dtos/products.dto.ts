97e7f068d5710a54f3c0c74711720329c1c7d954
hahs <alexandre_2420@outlook.com>
2022-11-09 15:04:26 -0500

    added: validators

--- src/dtos/products.dto.ts
+++ src/dtos/products.dto.ts
@@ -1,12 +1,44 @@
-export interface Product {
-  readonly id: string;
+// nest generate class products/dto/product.dto
+// nest g cl products/dto/product.dto
+
+import {
+  IsString,
+  IsNumber,
+  IsUrl,
+  IsNotEmpty,
+  IsOptional,
+  IsPositive,
+} from 'class-validator';
+
+import { PartialType } from '@nestjs/mapped-types';
+
+// ESTAS DOS LIBRERIAS SE DEBEN DESCARGAR POR SEPARADO DE NEST
+
+export class CreateProductDTO {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
   readonly price: number;
+
+  @IsNumber()
+  @IsPositive()
+  @IsNotEmpty()
   readonly stock: number;
+
+  @IsOptional()
+  @IsString()
   readonly description?: string;
+
+  @IsOptional()
+  @IsUrl()
   readonly image?: string;
 }
 
-export interface CreateProductDTO extends Omit<Product, 'id'> {}
-
-export interface UdpateProductDTO extends Partial<Product> {}
+export class UdpateProductDTO extends PartialType(CreateProductDTO) {
+  @IsString()
+  id?: string;
+}
