926c63dbe8e677e43feee6799cb5459a5ec34609
hahs <alexandre_2420@outlook.com>
2022-11-11 17:16:59 -0500

    created product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,8 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class ProductEntity {
+  @PrimaryGeneratedColumn()
   id: string;
+
+  @Column({ type: 'varchar', length: 255, unique: true })
   name: string;
+
+  @Column({ type: 'int' })
   price: number;
+
+  @Column({ type: 'int' })
   stock: number;
+
+  @Column({ type: 'text' })
   description?: string;
+
+  @Column({ type: 'varchar' })
   image?: string;
 }
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 
-import { CategoriesController } from './controllers/categories/categories.controller';
+//typeorm
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ProductEntity } from './entities/product.entity';
 import { ProductsController } from './controllers/products/products.controller';
+import { BrandsController } from './controllers/brands/brands.controller';
+import { CategoriesController } from './controllers/categories/categories.controller';
 import { ProductsService } from './services/products/products.service';
 import { CategoriesService } from './services/categories/categories.service';
 import { BrandsService } from './services/brands/brands.service';
-import { BrandsController } from './controllers/brands/brands.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductEntity])],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, CategoriesService, BrandsService],
   exports: [ProductsService],
